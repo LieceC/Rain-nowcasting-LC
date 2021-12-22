dd75db4ff9f8a53c27b246d3fa85a7ae9075ca0a
LieceC <33203860+LieceC@users.noreply.github.com>
2021-12-22 13:23:50 +0100

    Fix dataset and training

--- src/convLSTM/src/main.py
+++ src/convLSTM/src/main.py
@@ -3,39 +3,45 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
-from src.Models.ConvLSTM import ConvLSTM, torch
-from src.dataset import MeteoDataset
-from src.utils import tensorify
+from Models.ConvLSTM import ConvLSTM, torch
+from dataset import MeteoDataset
+from utils import tensorify
 
 
-def trainer(input_shape=(128, 128), input_dim=1, hidden_dim=64, kernel_size=3, input_length=5, output_length=15,
-            batch_size=1, epochs=50):
+def trainer(input_shape=(128, 128), input_dim=1, hidden_dim=64, kernel_size=3, input_length=2, output_length=2,
+            batch_size=2, epochs=20):
     torch.manual_seed(1)
     board = SummaryWriter()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     net = ConvLSTM(input_shape=input_shape,
                    input_dim=input_dim,
                    hidden_dim=hidden_dim,
-                   kernel_size=kernel_size)
+                   kernel_size=kernel_size,
+                   device=device)
+    net.to(device)
 
-    train = MeteoDataset(rain_dir='MeteoNet-Brest\\rainmap\\train', input_length=input_length,
-                         output_length=output_length)
-    val = MeteoDataset(rain_dir='MeteoNet-Brest\\rainmap\\val', input_length=input_length,
-                       output_length=output_length)
+    train = MeteoDataset(rain_dir='../data/rainmap/train', input_length=input_length,
+                         output_length=output_length, temporal_stride=input_length, dataset='train')
+    # train_sampler = CustomSampler(indices_except_undefined_sampler(train), train)
 
-    train_dataloader = DataLoader(train, batch_size=batch_size, shuffle=True)
+    val = MeteoDataset(rain_dir='../data/rainmap/val', input_length=input_length,
+                       output_length=output_length, temporal_stride=input_length, dataset='valid')
+    # val_sampler = CustomSampler(indices_except_undefined_sampler(val), val)
+
+    train_dataloader = DataLoader(train, batch_size=batch_size)
     valid_dataloader = DataLoader(val, batch_size=batch_size, shuffle=True)
 
-    optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
-    loss_f = torch.nn.CrossEntropyLoss()
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    net.to(device)
+    optimizer = torch.optim.Adam(net.parameters(), lr=10e-8, betas=(0.9, 0.999))
+    loss_f = torch.nn.BCEWithLogitsLoss()
 
     avg_train_losses = []
     avg_val_losses = []
 
-    # set checkpoint
+    thresholds_in_mmh = [0, 0.1, 1, 2.5]  # CRF over 1h
+    thresholds_in_cent_mm = [100 * k / 12 for k in thresholds_in_mmh]
+
+    # thresholds_normalized = np.log(np.array(thresholds_in_cent_mm) + 1) / train.norm_factor
+
     cur_epoch = 0
     for epoch in range(cur_epoch, epochs):
         print(epoch)
@@ -50,6 +56,8 @@ def trainer(input_shape=(128, 128), input_dim=1, hidden_dim=64, kernel_size=3, i
             optimizer.zero_grad()
             pred = tensorify(net(inputs)[0])
             pred = torch.squeeze(pred, 0)
+            # print(pred.size())
+            # print(targets.size())
             loss = loss_f(pred, targets)
             average_loss = loss.item() / batch_size
             train_losses.append(average_loss)
@@ -72,8 +80,8 @@ def trainer(input_shape=(128, 128), input_dim=1, hidden_dim=64, kernel_size=3, i
 
         for batch_idx, sample in enumerate(t):
             inputs, targets = sample['input'], sample['target']
-            inputs = inputs.to(device)
-            targets = targets.to(device)
+            inputs = inputs.to(device, dtype=torch.float32)
+            targets = targets.to(device, dtype=torch.float32)
             pred = tensorify(net(inputs)[0])
             pred = torch.squeeze(pred, 0)
             loss = loss_f(pred, targets)
@@ -87,8 +95,6 @@ def trainer(input_shape=(128, 128), input_dim=1, hidden_dim=64, kernel_size=3, i
         avg_val_losses.append(np.average(val_losses))
         board.add_scalar('TrainLoss', avg_train_losses[-1], epoch)
         board.add_scalar('ValidLoss', avg_val_losses[-1], epoch)
-
-        # use scheduler ?
 
     return net, avg_train_losses, avg_val_losses
 
